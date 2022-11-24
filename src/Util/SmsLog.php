a413c2cbbb282bf076b930e2919b1b3f8ca3ddf4
Tony Lu <lucups@gmail.com>
2022-11-24 21:04:10 +0800

    up

--- src/Util/SmsLog.php
+++ src/Util/SmsLog.php
@@ -3,10 +3,16 @@
 namespace Lucups\Cnsms\Util;
 
 use Lucups\Cnsms\Exception\SmsException;
-use Lucups\Cnsms\Sms;
 
 class SmsLog
 {
+    const LOG_FILE_NAME  = 'cnsms.log';
+    const DATETIME_STYLE = 'Y-m-d H:i:s';
+
+    const INFO  = 'INFO';
+    const WARN  = 'WARNING';
+    const ERROR = 'ERROR';
+
     /**
      * @var string 日志文件路径
      */
@@ -20,7 +26,7 @@ class SmsLog
     public static function initLogFilePath($logFilePath = null, $logFlag = true)
     {
         if (empty($logFilePath)) {
-            self::$LOGFILE_PATH = sys_get_temp_dir() . '/lucups-sms.log';
+            self::$LOGFILE_PATH = sys_get_temp_dir() . DIRECTORY_SEPARATOR . self::LOG_FILE_NAME;
         } else {
             self::$LOGFILE_PATH = $logFilePath;
         }
@@ -36,22 +42,22 @@ class SmsLog
             throw new SmsException('Logfile Path  is empty.');
         }
 
-        $prefix = date('Y-m-d H:i:s') . ' ' . $level . ' ';
+        $prefix = date(self::DATETIME_STYLE) . ' ' . $level . ' ';
         file_put_contents($prefix . self::$LOGFILE_PATH, $text . "\n", FILE_APPEND);
     }
 
     public static function info($text)
     {
-        self::log('INFO', $text);
+        self::log(self::INFO, $text);
     }
 
     public static function warn($text)
     {
-        self::log('WARNING', $text);
+        self::log(self::WARN, $text);
     }
 
     public static function error($text)
     {
-        self::log('ERROR', $text);
+        self::log(self::ERROR, $text);
     }
 }
