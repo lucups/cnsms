afa79dded34c74cc6c24a9375ddb64fbdc745214
Tony Lu <lucups@gmail.com>
2025-09-19 16:47:31 +0800

    feat(sms): 支持短信宝余额查询并优化短信发送功能
    - 新增 balance 方法，支持查询短信宝账户余额和短信余量
    - 优化 send 方法，支持模板变量替换
    - 更新 README 文档，添加余额查询示例和说明
    - 重构部分代码，提高可维护性和可读性

--- src/Client/SmsBaoClient.php
+++ src/Client/SmsBaoClient.php
@@ -8,9 +8,24 @@ namespace Lucups\Cnsms\Client;
  */
 class SmsBaoClient implements SmsClient
 {
+    const BASE_API_URL = 'https://api.smsbao.com/';
+    const BASE_WWW_URL = 'https://www.smsbao.com/';
+
     private $username;
     private $password;
     private $apiKey;
+
+    const STATUS_MAP = [
+        "0"  => "短信发送成功",
+        "-1" => "参数不全",
+        "-2" => "服务器空间不支持,请确认支持curl或者fsocket，联系您的空间商解决或者更换空间！",
+        "30" => "密码错误",
+        "40" => "账号不存在",
+        "41" => "余额不足",
+        "42" => "帐户已过期",
+        "43" => "IP地址限制",
+        "50" => "内容含有敏感词",
+    ];
 
     public function __construct($username, $password, $apiKey = null)
     {
@@ -27,24 +42,42 @@ class SmsBaoClient implements SmsClient
      */
     public function send($phone, string $templateCode, array $data = []): array
     {
-        $statusMap = [
-            "0"  => "短信发送成功",
-            "-1" => "参数不全",
-            "-2" => "服务器空间不支持,请确认支持curl或者fsocket，联系您的空间商解决或者更换空间！",
-            "30" => "密码错误",
-            "40" => "账号不存在",
-            "41" => "余额不足",
-            "42" => "帐户已过期",
-            "43" => "IP地址限制",
-            "50" => "内容含有敏感词",
-        ];
+        $user = $this->username;                                                   //短信平台帐号
+        $pass = empty($this->password) ? $this->apiKey : md5($this->password);     //短信平台密码
 
-        $smsapi     = "https://api.smsbao.com/";
-        $user       = $this->username;                                                   //短信平台帐号
-        $pass       = empty($this->password) ? $this->apiKey : md5($this->password);     //短信平台密码
-        $content    = $templateCode;                                                     //要发送的短信内容
-        $sendurl    = $smsapi . "sms?u=" . $user . "&p=" . $pass . "&m=" . $phone . "&c=" . urlencode($content);
-        $resultCode = file_get_contents($sendurl);
-        return ['code' => $resultCode, 'msg' => $statusMap[$resultCode]];
+        $content = $templateCode; //要发送的短信内容
+        foreach ($data as $key => $val) {
+            $content = str_replace('{' . $key . '}', $val, $content);
+        }
+
+        $sendUrl = self::BASE_API_URL . "sms?u=" . $user . "&p=" . $pass . "&m=" . $phone . "&c=" . urlencode($content);
+        $result  = file_get_contents($sendUrl);
+
+        $resultParts = explode("\n", $result);
+        $resultCode  = $resultParts[0];
+
+        return ['code' => $resultCode, 'msg' => self::STATUS_MAP[$resultCode]];
+    }
+
+    public function balance(): array
+    {
+        $user    = $this->username;                                                   //短信平台帐号
+        $pass    = empty($this->password) ? $this->apiKey : md5($this->password);     //短信平台密码
+        $sendUrl = self::BASE_WWW_URL . "query?u=" . $user . "&p=" . $pass;
+        $result  = file_get_contents($sendUrl);
+
+        $resultParts = explode("\n", $result);
+        $resultCode  = $resultParts[0];
+
+        $dataParts = explode(',', $resultParts[1]);
+        $balance   = $dataParts[0];
+        $leftNum   = $dataParts[1];
+
+        return [
+            'code'    => $resultCode,
+            'msg'     => $resultCode == 0 ? 'success' : 'fail',
+            'leftNum' => $leftNum,
+            'balance' => $balance,
+        ];
     }
 }
